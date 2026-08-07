0e3515ab5f7338c76f199f5af8bbdcaeac379ab6
leofkz12 <leozgrautecn@exemplo.com>
2026-08-07 20:09:19 -0300

    update

--- dados.js
+++ dados.js
@@ -21,7 +21,7 @@ const hqs = {
     capa: "assets/capas/frieren.jpg",
     genero: "manga",
     lancamento: "28/4/2020",
-    resumo: "A sinopse acompanha a maga elfa Frieren, que, após uma década de jornada para derrotar o Rei Demônio junto ao herói humano Himmel, retorna à vida comum.  Devido à sua longa vida élfica, Frieren não compreende a efemeridade da vida humana e testemunha seus companheiros envelhecerem e morrerem. Anos após a morte de Himmel, ela embarca em uma nova jornada com sua aprendiz humana, Fern, e posteriormente com o guerreiro Stark.",
+    resumo: "A história acompanha a maga elfa Frieren, que, após uma década de jornada para derrotar o Rei Demônio junto ao herói humano Himmel, retorna à vida comum.  Devido à sua longa vida élfica, Frieren não compreende a efemeridade da vida humana e testemunha seus companheiros envelhecerem e morrerem. Anos após a morte de Himmel, ela embarca em uma nova jornada com sua aprendiz humana, Fern, e posteriormente com o guerreiro Stark.",
     capitulos: {
       "cap-1": 1
     }
@@ -31,7 +31,7 @@ const hqs = {
     capa: "assets/capas/miles-morales.jpg",
     genero: "hq",
     lancamento: "28/8/2019",
-    resumo: "A sinopse da história foca no Miles Morales enfrentando o Escorpião (Mac Gargan), que possui o simbionte Venom.  Quando uma horda de acólitos do Carnificina ataca Nova York, Miles é infectado pelo simbionte vermelho e possuído, transformando-se temporariamente em um vilão. ",
+    resumo: "A história foca no Miles Morales enfrentando o Escorpião (Mac Gargan), que possui o simbionte Venom.  Quando uma horda de acólitos do Carnificina ataca Nova York, Miles é infectado pelo simbionte vermelho e possuído, transformando-se temporariamente em um vilão. ",
     capitulos: {
       "cap-1": 21,
       "cap-2": 1
@@ -53,7 +53,7 @@ const hqs = {
     capa: "assets/capas/overlord.jpg",
     genero: "manga",
     lancamento: "20/1/2021",
-    resumo: "Sinopse: A história acompanha Momonga, um jogador dedicado do MMORPG Yggdrasil, que decide não deslogar quando os servidores são encerrados.  Ele se vê transportado para um novo mundo e transformado em seu avatar de esqueleto, o poderoso feiticeiro Ainz Ooal Gown, enquanto seus NPCs ganham vida própria e lealdade absoluta. ",
+    resumo: "A história acompanha Momonga, um jogador dedicado do MMORPG Yggdrasil, que decide não deslogar quando os servidores são encerrados.  Ele se vê transportado para um novo mundo e transformado em seu avatar de esqueleto, o poderoso feiticeiro Ainz Ooal Gown, enquanto seus NPCs ganham vida própria e lealdade absoluta. ",
     capitulos: {
       "cap-1": 1,
       "cap-2": 1
@@ -64,7 +64,7 @@ const hqs = {
     capa: "assets/capas/spider-man2099.jpg",
     genero: "hq",
     lancamento: "4/5/2022",
-    resumo: "Sinopse: Miguel O'Hara (Homem-Aranha 2099) retorna ao seu presente para investigar o evento cósmico e proteger o novo paraíso da ameaçadora Cabal, liderada por Norman Osborn.  A série apresenta versões futuras de personagens como Soldado Invernal 13, Loki, Motoqueiro Fantasma e uma nova formação de X-Men 2099, além de introduzir os Novos Vingadores de 2099 liderados pelo Cavaleiro da Lua.",
+    resumo: "Miguel O'Hara (Homem-Aranha 2099) retorna ao seu presente para investigar o evento cósmico e proteger o novo paraíso da ameaçadora Cabal, liderada por Norman Osborn.  A série apresenta versões futuras de personagens como Soldado Invernal 13, Loki, Motoqueiro Fantasma e uma nova formação de X-Men 2099, além de introduzir os Novos Vingadores de 2099 liderados pelo Cavaleiro da Lua.",
     capitulos: {
       "cap-1": 1,
       "cap-2": 1
